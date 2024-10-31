f479bb5716a046814a8dac66fa542e5428156c58
Edwell22 <113574992+Edwell22@users.noreply.github.com>
2024-10-31 02:28:29 +0200

    update

--- nextjs-dashboard/app/dashboard/customers/page.tsx
+++ nextjs-dashboard/app/dashboard/customers/page.tsx
@@ -1,3 +1,10 @@
+import { Metadata } from 'next';
+
+
+export const metadata: Metadata = {
+  title: 'Customers',
+};
+
 export default function Page() {
-    return <p>Welcome Customers Page</p>;
+    return <p>Customers Page</p>;
   }
